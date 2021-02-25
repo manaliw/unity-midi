4d8252ac7e3239eee60f925f7b2e6c6155e02a64
manaliw <manaliwarwandkar@gmail.com>
2021-02-25 14:22:39 +0100

    First Commit
    Whole of Unity file

---
+++ 3D Mandala Effect/Assets/rotate.cs
@@ -0,0 +1,65 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using MidiJack;
+
+public class rotate : MonoBehaviour
+{
+    public GameObject originalObject;
+
+    public Light lightA, lightB;
+
+    private List<GameObject> clonedObjects = new List<GameObject>();
+
+
+    private float time = 0;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        for (int x = -5; x <= 5; x++) {
+            for (int z = -5; z <= 5; z++) 
+            {
+                GameObject clonedObject = Instantiate(
+                    originalObject, 
+                    new Vector3(x, 0, z), 
+                    Quaternion.identity
+                );
+
+                clonedObject.transform.parent = this.transform;
+                clonedObject.SetActive(true);
+
+                clonedObjects.Add(clonedObject);
+            }
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+        float xRotation = MidiMaster.GetKnob(0x0A, 0);
+        float yRotation = MidiMaster.GetKnob(0x4A, 0);
+        float zRotation = MidiMaster.GetKnob(0x47, 0);
+
+        foreach (GameObject clonedObject in clonedObjects)
+        {
+            transform.Rotate(xRotation, yRotation, zRotation);
+        }
+
+        bool randomize = MidiMaster.GetKeyDown(0x2C);
+
+        if (randomize) {
+            float randomHueA = Random.Range(0f, 1f);
+            float randomHueB = Random.Range(0f, 1f);
+            float randomSatA = Random.Range(0f, 1f);
+            float randomSatB = Random.Range(0f, 1f);
+
+            lightA.color = Color.HSVToRGB(randomHueA, randomSatA, 1f);
+            lightB.color = Color.HSVToRGB(randomHueB, randomSatB, 1f);
+        }
+           
+        
+    }
+}
+
---
+++ 3D Mandala Effect/Assets/rotateobject.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using MidiJack;
+
+public class rotateobject : MonoBehaviour
+{
+    public GameObject originalObject;
+
+    public Light lightA, lightB;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+
+        originalObject.transform.parent = this.transform;
+        originalObject.SetActive(true);
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+        //float speed = MidiMaster.GetKnob(0x47, 0) / 10;
+        float xRotation = MidiMaster.GetKnob(0x0A, 0);
+        float yRotation = MidiMaster.GetKnob(0x4A, 0);
+        float zRotation = MidiMaster.GetKnob(0x47, 0);
+
+        transform.Rotate(xRotation, yRotation, zRotation);  
+
+        bool randomize = MidiMaster.GetKeyDown(0x2C);
+
+        if (randomize) {
+            float randomHueA = Random.Range(0f, 1f);
+            float randomHueB = Random.Range(0f, 1f);
+            float randomSatA = Random.Range(0f, 1f);
+            float randomSatB = Random.Range(0f, 1f);
+
+            lightA.color = Color.HSVToRGB(randomHueA, randomSatA, 1f);
+            lightB.color = Color.HSVToRGB(randomHueB, randomSatB, 1f);
+        } 
+    }
+}
